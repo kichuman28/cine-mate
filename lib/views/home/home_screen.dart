410b887bcb99ff6e8848f4294f710600eb880cde
Adwaith Jayasankar <kichu31084@gmail.com>
2025-06-07 02:03:21 +0530

    Update: Wrote code for bottom navbar

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,1 +1,62 @@
 // Created by: Adwaith Jayasankar, Created at: 07-06-2025 01:16
+import 'package:flutter/material.dart';
+import 'package:cine_mate/views/movie/movies_view.dart';
+import 'package:cine_mate/views/group/groups_view.dart';
+import 'package:cine_mate/views/profile/profile_view.dart';
+import 'package:cine_mate/views/home/home_view.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeView(),
+    MoviesView(),
+    GroupsView(),
+    ProfileView(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Movie',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Groups',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          )
+        ],
+      ),
+    );
+  }
+}
