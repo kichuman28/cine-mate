49390f6fc4a988650919ff51b7d2d40708bc88da
Adwaith Jayasankar <kichu31084@gmail.com>
2025-06-07 02:03:49 +0530

    Update: Wrote dummy code for models

--- lib/models/group_model.dart
+++ lib/models/group_model.dart
@@ -1,1 +1,7 @@
 // Created by: Adwaith Jayasankar, Created at: 07-06-2025 01:11
+class Group {
+  final String id;
+  final String name;
+
+  Group({required this.id, required this.name});
+}
--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -1,1 +1,7 @@
 // Created by: Adwaith Jayasankar, Created at: 07-06-2025 01:11
+class Movie {
+  final String id;
+  final String title;
+
+  Movie({required this.id, required this.title});
+}
