047941f62ac1c089fce819b4970c7431db49ed79
Adwaith Jayasankar <kichu31084@gmail.com>
2025-06-07 12:08:29 +0530

    Update: Implemented an auth wrapper that checks if I'm logged in or not

--- lib/widgets/auth_wrapper.dart
+++ lib/widgets/auth_wrapper.dart
@@ -1,5 +1,8 @@
 // Created by: Adwaith Jayasankar, Created at: 07-06-2025 10:59
+import 'package:cine_mate/screens/auth/login_screen.dart';
+import 'package:cine_mate/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthWrapper extends StatefulWidget {
   const AuthWrapper({super.key});
@@ -11,6 +14,21 @@ class AuthWrapper extends StatefulWidget {
 class _AuthWrapperState extends State<AuthWrapper> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (snapshot.hasData) {
+          return const HomeScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
+    );
   }
 }
