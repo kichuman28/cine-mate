4a5a8effd9c97dc04983426204052ced7a56fe3b
Adwaith Jayasankar <kichu31084@gmail.com>
2025-06-07 12:03:41 +0530

    Update: Added logout button and redirection to login screen in profile screen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,5 +1,6 @@
 // Created by: Adwaith Jayasankar, Created at: 07-06-2025 01:18
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -9,8 +10,29 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  Future<void> _logout() async {
+    await FirebaseAuth.instance.signOut();
+    if (!mounted) return;
+    Navigator.pushReplacementNamed(context, '/login');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: _logout,
+          label: const Text("Logout"),
+          icon: const Icon(Icons.logout),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.deepPurple,
+            foregroundColor: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
